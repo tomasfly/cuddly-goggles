8fd66f88423f86c44a89027db567b149fd743502
Tomas Fleiderman <tomas.fleiderman@newsignature.com>
2024-09-24 10:55:18 +0200

    first commit

---
+++ src/main/java/seleniumjava/etsy/pages/AdvancedSearch.java
@@ -0,0 +1,38 @@
+package seleniumjava.etsy.pages;
+
+import org.jbehave.web.selenium.FluentWebDriverPage;
+import org.jbehave.web.selenium.WebDriverProvider;
+
+import static org.openqa.selenium.By.className;
+import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.By.xpath;
+
+public class AdvancedSearch extends FluentWebDriverPage {
+
+    public AdvancedSearch(WebDriverProvider webDriverProvider) {
+        super(webDriverProvider);
+    }
+
+    public void go() {
+        get("http://www.etsy.com/search_advanced.php");
+    }
+
+    public void go(String section) {
+        go();
+        link(xpath("@title = '" + section + "'")).click();
+    }
+
+    public void search(String thing) {
+        input(id("search-query")).sendKeys(thing);
+        input(id("search_submit")).click();
+    }
+
+    public void subCategory(String subCategory) {
+        select(className("handmade")).selectByValue(subCategory.toLowerCase());
+    }
+
+    public void searchFor(String thing) {
+        input(id("search_query")).sendKeys(thing).submit();
+    }
+    
+}
